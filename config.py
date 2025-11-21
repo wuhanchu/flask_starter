d4dffd8d2c9f56e981accaa0620389c2738dd7ff
wuhanchu <whcwuhanchu@gmail.com>
2025-11-21 10:28:37 +0800

    修复配置

--- config.py
+++ config.py
@@ -6,23 +6,23 @@ class Config:
 
     # 项目配置
     PRODUCT_KEY = "flask_starter"
-    RUN_PORT = os.environ.get("RUN_PORT", 5000)
+    RUN_PORT = os.getenv("RUN_PORT", 5000)
 
     # 启用的扩展
     ENABLED_EXTENSION = ["loguru", "sentry"]
 
     # 用户认证
-    USER_AUTH_URL = os.environ.get("USER_AUTH_URL", "http://127.0.0.1:5000")
+    USER_AUTH_URL = os.getenv("USER_AUTH_URL", "http://127.0.0.1:5000")
 
     # Sentry错误跟踪配置
-    SENTRY_DS = os.environ.get("SENTRY_DS")
+    SENTRY_DS = os.getenv("SENTRY_DS")
 
     # 启用的模块
     ENABLED_MODULE = []
 
     # 数据库配置
     # DB_SCHEMA = "flask_starter"
-    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")  # 数据库连接URI
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")  # 数据库连接URI
 
 
 class DevelopmentConfig(Config):
